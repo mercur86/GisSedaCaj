b2e34c30e3b3f0f367490d64ccafa8022e3d5abc
alexis <alexistr86@gmail.com>
2025-11-25 14:16:21 -0500

    correccion inicial

--- gis_backend/src/dbconexion.js
+++ gis_backend/src/dbconexion.js
@@ -1,15 +1,15 @@
 const pgp = require('pg-promise')();
-// const host = '176.52.142.84',
-//     port = 9876,
-//     database = 'sisgeco_comercial',
-//     user = 'sigiluser',
-//     password = 'ñ6u3$&2C0!j4@cG';
-
-const host = '44.193.209.119',
-    port = 5436,
+const host = '176.52.142.84',
+    port = 9876,
     database = 'sisgeco_comercial',
-    user = 'postgres',
-    password = '15#YQd}4C[/X';
+    user = 'sigiluser',
+    password = 'ñ6u3$&2C0!j4@cG';
+
+// const host = '44.193.209.119',
+//     port = 5436,
+//     database = 'sisgeco_comercial',
+//     user = 'postgres',
+//     password = '15#YQd}4C[/X';
 
 const db = pgp(`postgres://${user}:${password}@${host}:${port}/${database}`);
 
